8e312855ff2537abb4ce98259dabfc19898bf88a
Elvin Efendi <elvin.efendiyev@gmail.com>
2020-12-18 10:49:50 -0500

    also need to adjust spec

--- spec/resty/global_throttle_spec.lua
+++ spec/resty/global_throttle_spec.lua
@@ -14,21 +14,21 @@ describe("global_throttle", function()
     end)
 
     it("requires store param to have provider attribute defined", function()
-      local my_throttle, err = global_throttle.new(100, 5, {})
+      local my_throttle, err = global_throttle.new(100, 5, ngx.shared.decision_cache, {})
 
       assert.is_nil(my_throttle)
       assert.are.equals("error initiating the store: 'provider' attribute is missing", err)
     end)
 
     it("returns global throttle instance for Lua shared dict backend store", function()
-      local my_throttle, err = global_throttle.new(100, 5, { provider = "shared_dict", name = "my_global_throttle" } )
+      local my_throttle, err = global_throttle.new(100, 5, ngx.shared.decision_cache, { provider = "shared_dict", name = "my_global_throttle" } )
 
       assert.is_nil(err)
       assert.is_not_nil(my_throttle)
     end)
 
     it("returns global throttle instance for memcached backend store", function()
-      local my_throttle, err = global_throttle.new(100, 5,
+      local my_throttle, err = global_throttle.new(100, 5, ngx.shared.decision_cache,
         { provider = "memcached", host = os.getenv("MEMCACHED_HOST"), port = "11211" } )
 
       assert.is_nil(err)
@@ -42,7 +42,7 @@ describe("global_throttle", function()
 
       before_each(function()
         local err
-        my_throttle, err = global_throttle.new(10, 2,
+        my_throttle, err = global_throttle.new(10, 2, ngx.shared.decision_cache,
           { provider = "shared_dict", name = "my_global_throttle" } )
         assert.is_nil(err)
       end)
@@ -54,25 +54,22 @@ describe("global_throttle", function()
       it("does not throttle when within limits", function()
         local exceeding_limit, err
         for i=1,10,1 do
-          exceeding_limit, err = my_throttle:process("client1")
+          exceeding_limit = my_throttle:process("client1")
         end
 
-        assert.is_nil(err)
         assert.is_false(exceeding_limit)
       end)
 
       it("throttles when over limit", function()
-        local exceeding_limit, err
+        local exceeding_limit
         for i=1,10,1 do
-          exceeding_limit, err = my_throttle:process("client1")
+          exceeding_limit = my_throttle:process("client1")
         end
 
-        assert.is_nil(err)
         assert.is_false(exceeding_limit)
 
-        exceeding_limit, err = my_throttle:process("client1")
+        exceeding_limit = my_throttle:process("client1")
 
-        assert.is_nil(err)
         assert.is_true(exceeding_limit)
       end)
 
